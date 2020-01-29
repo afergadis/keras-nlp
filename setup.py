efb8162bec0b457f401545e6028bc02a1d00165b
Aris Fergadis <aris.fergadis@protonmail.com>
2020-01-29 11:31:10 +0200

    Update setup dependencies.

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='keras_nlp',
-    version='0.1',
+    version='0.1.3',
     url='',
     license='',
     author='Aris Fergadis',
     author_email='aris.fergadis@protonmail.com',
     description='Library for NLP processing for Keras models.',
-    install_requires=['scikit-learn', 'numpy', 'keras', 'tensorflow<2.0'],
+    install_requires=['scikit-learn', 'numpy', 'keras', 'tensorflow',
+                      'joblib', 'jsonpickle'],
     packages=find_packages(),
     test_suite='keras_nlp.tests',
 )
